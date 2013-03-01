8ed61e21f38806fb411530c1e52847f7152f28ce
Joe.Winter <joe.winter@sungard.com>
2013-03-01 16:10:18 -0500

    Using clj-aouth

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject tweetah "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Repl twitter client"
+  :url "http://github.com/jwinter/tweetah"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-oauth "1.4.0"]
+                 ])
--- src/tweetah/core.clj
+++ src/tweetah/core.clj
@@ -1,6 +1,13 @@
-(ns tweetah.core)
+(ns tweetah.core
+  (:require [oauth.client :as oauth])
+  )
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn init []
+  (def creds (load-file "creds.twitter"))
+  
+  )
+
+;(def consumer [consumer-token consumer-token-secret]
+;  (oauth/make-consumer
+  
